7a6c276592eb8e132a02556fb1e0d4e65535b954
W-Mai <1341398182@qq.com>
2022-10-26 02:45:43 +0800

    ❤️(Qin): support `subscribe` and setter chain
    Signed-off-by: W-Mai <1341398182@qq.com>

--- source/Qin.cpp
+++ source/Qin.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Qin.h"
+
+void operator<<(QinBase& l, QinBase& r) {
+    l.bind(&r);
+}
+
+void operator<<(QinBase& l, QinBase* r) {
+    l.bind(r);
+}
+
+void operator<<(QinBase* l, QinBase& r) {
+    l->bind(&r);
+}
+
+void QinBase::bind(QinBase* src) {
+    auto& src_zong = src->Zong;
+    if (std::find(src_zong.begin(), src_zong.end(), this) != src_zong.end()) {
+        return;
+    }
+
+    src_zong.push_back(this);
+}
--- source/Qin.h
+++ source/Qin.h
@@ -5,28 +5,56 @@
 #ifndef ZONGHENG_QIN_H
 #define ZONGHENG_QIN_H
 
+#include <vector>
+
 class QinBase {
+protected:
+    using Zong_t = std::vector<QinBase*>;
+    //    using Heng = std::vector<QinBase*>;
+    Zong_t Zong;
+
 public:
-    virtual void print() {};
+    friend void operator<<(QinBase& l, QinBase& r);
+    friend void operator<<(QinBase& l, QinBase* r);
+    friend void operator<<(QinBase* l, QinBase& r);
+
+    void bind(QinBase* src);
 };
 
 template<class T>
 class Qin : public QinBase {
-private:
     T value;
 
 public:
-    explicit Qin(T v)
-        : value(v) { }
+    Qin(Qin<T>& v) noexcept
+        : value(std::forward<T>(v.value)) { }
 
-    void print() override;
+    explicit Qin(T& v)
+        : value(std::forward<T>(v)) { }
+
+    explicit Qin(T&& v)
+        : value(std::forward<T>(v)) { }
+
+    void set(T&& val) {
+        value = std::forward<T>(val);
+        for (QinBase* qin : Zong) {
+            static_cast<Qin<T>*>(qin)->set(std::forward<T>(val));
+        }
+    }
+
+    T get() {
+        return value;
+    }
+
+    Qin<T>& operator=(T& val) {
+        set(std::forward<T>(val));
+        return *this;
+    }
+
+    Qin<T>& operator=(T&& val) {
+        set(std::forward<T>(val));
+        return *this;
+    }
 };
-
-#include <iostream>
-
-template<class T>
-void Qin<T>::print() {
-    std::cout << value << std::endl;
-}
 
 #endif // ZONGHENG_QIN_H
