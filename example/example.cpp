08d22cee2ee22c46ce0bd83b06e768b900235f49
W-Mai <1341398182@qq.com>
2022-10-26 02:46:53 +0800

    🆕(example): update example
    Signed-off-by: W-Mai <1341398182@qq.com>

--- example/example.cpp
+++ example/example.cpp
@@ -1,15 +1,33 @@
 #include "Qin.h"
 #include <vector>
 
-int main() {
-
-    for (auto* _ : std::vector<QinBase*> {
-             new Qin<int>(114514),
-             new Qin<std::string>("Hello, "),
-             new Qin<const char*>("Qin"),
-             new Qin<double>(2333) }) {
-        _->print();
+template<typename T>
+void display(std::vector<QinBase*>& vec) {
+    for (QinBase* item : vec) {
+        std::cout << static_cast<Qin<T>*>(item)->get() << " ";
     }
+    std::cout << std::endl;
+}
+
+int main() {
+    auto vec = std::vector<QinBase*> {};
+
+    for (int i = 0; i < 10; i++) {
+        vec.push_back(new Qin<int>(i));
+    }
+    display<int>(vec);
+
+    for (int i = 1; i < vec.size(); ++i) {
+        vec[i - 1] << *vec[i];
+    }
+    display<int>(vec);
+
+    int a                    = 4;
+    *(Qin<int>*)(vec.back()) = a;
+    display<int>(vec);
+
+    *(Qin<int>*)(vec.back()) = 114514;
+    display<int>(vec);
 
     return 0;
 }
