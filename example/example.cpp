f16745704c9169ec79d063a9843d900b79382ac9
W-Mai <1341398182@qq.com>
2022-10-30 00:13:38 +0800

    🆕(example): update example
    Signed-off-by: W-Mai <1341398182@qq.com>

--- example/example.cpp
+++ example/example.cpp
@@ -11,6 +11,9 @@ void display(std::vector<QinBase*>& vec) {
 }
 
 void test_chain() {
+    std::cout << "\n"
+              << __func__ << std::endl;
+
     auto vec = std::vector<QinBase*> {};
 
     for (int i = 0; i < 10; i++) {
@@ -39,17 +42,43 @@ void test_chain() {
 }
 
 void test_binary_ops() {
-    auto p  = new Qin<int> { 10 };
-    auto q  = new Qin<int> { 30 };
-    auto zh = &q->lian<std::multiplies<int>>(p);
-    std::cout << *zh << std::endl;
+    std::cout << "\n"
+              << __func__ << std::endl;
+
+    auto p = new Qin<int> { 10 };
+    auto q = new Qin<int> { 30 };
+    auto r = new Qin<int> { 37 };
+
+    auto zh = *q + *p + *r;
+    std::cout << zh->get() << std::endl;
+    auto zh2 = *q * *p * *r;
 
     *q = 3;
-    std::cout << *zh << std::endl;
+    std::cout << zh->get() << std::endl;
+    std::cout << zh2->get() << std::endl;
+}
+
+void test_binary_string() {
+    std::cout << "\n"
+              << __func__ << std::endl;
+
+    auto p = new Qin<std::string> { "Hello " };
+    auto q = new Qin<std::string> { "World " };
+    auto r = new Qin<std::string> { "!" };
+
+    auto zh = *p + *q + *r;
+    std::cout << zh->get() << std::endl;
+    std::cout << zh->get() << std::endl;
+    std::cout << zh->get() << std::endl;
+
+    auto s = std::string("China ");
+    *q     = s;
+    std::cout << zh->get() << std::endl;
 }
 
 int main() {
     test_chain();
     test_binary_ops();
+    test_binary_string();
     return 0;
 }
