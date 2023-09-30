0d2d119bafcb3f2c021c49a222a090f520f48daf
xinbingnan <xinbingnan@xiaomi.com>
2023-10-01 00:11:12 +0800

    🚧(tests): refactor tests: Replaced binary int ops with string operations
    Renamed the `test_binary_int_ops` to `test_binary_string_ops` to better reflect the operations being performed in the functions. The implementation of the tests in both plus_test.cpp and plus_string_test.cpp were also changed to adapt string operations instead of integer operations for clear indication of the testing process. Also, a new function `test_string_vec` is included in plus_string_test.cpp to extend the test cases by involving vector string operations.
    
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

--- test/plus_string_test.cpp
+++ test/plus_string_test.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include <vector>
 
-int test_binary_int_ops() {
+int test_binary_string_ops() {
     auto p = Qin<std::string>::make("Hello ");
     auto q = Qin<std::string>::make("World ");
     auto r = Qin<std::string>::make("!");
@@ -23,9 +23,32 @@ int test_binary_int_ops() {
     return 0;
 }
 
+int test_string_vec() {
+    auto list = {
+        std::string { "Hello " },
+        std::string { "World " },
+        std::string { "!" }
+    };
+
+    auto p = Qin<std::vector<std::string>>::make(list);
+
+    p->getter([](const std::vector<std::string>& vec) -> std::vector<std::string> {
+        std::string ret;
+        for (const auto& s : vec) {
+            ret += s;
+        }
+        return std::vector<std::string> { ret };
+    });
+
+    ASSERT_S(p->get()[0], std::string("Hello World !"));
+
+    return 0;
+}
+
 int main() {
     auto tests = {
-        test_binary_int_ops(),
+        test_binary_string_ops(),
+        test_string_vec()
     };
 
     return !std::all_of(tests.begin(), tests.end(), [](int val) {
